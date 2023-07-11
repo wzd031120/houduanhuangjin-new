b26b2257994adbaa12483385590afea3e3166676
wzd031120 <2401176210@qq.com>
2023-07-11 21:59:39 +0800

    分类管理之前的代码

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.result.PageResult;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+    //新增员工
+    void save(EmployeeDTO employeeDTO);
+    //分页查询
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void StartOrStop(Integer status, Long id);
+}
